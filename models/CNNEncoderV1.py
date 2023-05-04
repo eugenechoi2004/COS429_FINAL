2bf04a609bc6f73a2cd4b7700cc233d43eb09b3b
Eugene Choi <eugenechoi2004@gmail.com>
2023-05-04 15:52:48 -0400

    slurm

---
+++ models/CNNEncoderV1.py
@@ -0,0 +1,93 @@
+import torch.nn as nn
+from torch import optim
+from torch.autograd import Variable
+from torchvision import datasets
+from torchvision.transforms import ToTensor
+from torch.utils.data import DataLoader
+import torch
+
+class CNNEncoderV1(nn.Module):
+    def __init__(self):
+        super(CNNEncoderV1, self).__init__()
+        self.conv1 = nn.Sequential(         
+            nn.Conv2d(
+                in_channels=1,              
+                out_channels=16,            
+                kernel_size=5,              
+                stride=1,                   
+                padding=2,                  
+            ),                              
+            nn.ReLU(),                      
+            nn.MaxPool2d(kernel_size=2),    
+        )
+        self.conv2 = nn.Sequential(         
+            nn.Conv2d(16, 32, 5, 1, 2),     
+            nn.ReLU(),                      
+            nn.MaxPool2d(2),                
+        )
+        # fully connected layer, output 10 classes
+        self.out = nn.Linear(32 * 7 * 7, 10)
+
+    def forward(self, x):
+        x = self.conv1(x)
+        x = self.conv2(x)
+        # flatten the output of conv2 to (batch_size, 32 * 7 * 7)
+        x = x.view(x.size(0), -1)
+        output = self.out(x)
+        return output, x    # return x for visualization
+
+def train(num_epochs, cnn, loaders):
+    loss_func = nn.CrossEntropyLoss()   
+    optimizer = optim.Adam(cnn.parameters(), lr = 0.01)   
+    cnn.train()
+    # Train the model
+    total_step = len(loaders['train'])
+    for epoch in range(num_epochs):
+        for i, (images, labels) in enumerate(loaders['train']):
+            # gives batch data, normalize x when iterate train_loader
+            b_x = Variable(images)   # batch x
+            b_y = Variable(labels)   # batch y
+            output = cnn(b_x)[0]               
+            loss = loss_func(output, b_y)
+            # clear gradients for this training step   
+            optimizer.zero_grad()           
+            # backpropagation, compute gradients 
+            loss.backward()    
+            # apply gradients             
+            optimizer.step()                
+            if (i+1) % 100 == 0:
+                print ('Epoch [{}/{}], Step [{}/{}], Loss: {:.4f}' 
+                       .format(epoch + 1, num_epochs, i + 1, total_step, loss.item()))
+
+
+
+train_data = datasets.MNIST(
+    root = 'data',
+    train = True,                         
+    transform = ToTensor(), 
+    download = True,            
+)
+test_data = datasets.MNIST(
+    root = 'data', 
+    train = False, 
+    transform = ToTensor()
+)
+
+loaders = {
+    'train' : torch.utils.data.DataLoader(train_data, 
+                                          batch_size=100, 
+                                          shuffle=True, 
+                                          num_workers=1),
+    
+    'test'  : torch.utils.data.DataLoader(test_data, 
+                                          batch_size=100, 
+                                          shuffle=True, 
+                                          num_workers=1),
+}
+
+cnn = CNNEncoderV1()
+num_epochs = 10
+
+
+train(num_epochs, cnn, loaders)
+print(cnn)
